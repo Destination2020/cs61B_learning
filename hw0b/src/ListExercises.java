0f0dbc70e4e9d685aa2c290e7db7a1c4109663f9
NikaNI <172894792+Destination2020@users.noreply.github.com>
2025-08-04 23:31:55 +0800

    List and Map Exercises finished

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,29 +1,51 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
 	public static int sum(List<Integer> L) {
-        // TODO: Fill in this function.
-        return 0;
+        int sum_num = 0;
+
+        for (int i : L){
+            sum_num = i + sum_num;
+        }
+        return sum_num;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> res = new ArrayList<>();
+        for (int i : L){
+            if (i % 2 == 0){
+                res.add(i);
+            }
+        }
+        return res;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> res = new ArrayList<>();
+        for (int i : L1){
+            if (L2.contains(i)){
+                res.add(i);
+            }
+        }
+        return res;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        // TODO: Fill in this function.
-        return 0;
+        int count = 0;
+        for (String word : words){
+            for (int i = 0; i < word.length(); i++){
+                if (word.charAt(i) == c){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
